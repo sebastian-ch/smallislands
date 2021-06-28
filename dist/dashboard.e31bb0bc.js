6443e488ec58707e0f960067d9705cb9b72bc331
Sebastian Hancock <sebastian.hancock@tum.de>
2021-06-28 12:17:51 -0400

    update

--- dist/dashboard.e31bb0bc.js
+++ dist/dashboard.e31bb0bc.js
@@ -43311,7 +43311,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54584" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55953" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
